b74fe2e3c008c3755e795a2ad4d35f37d7fb2e95
Jason <jason@nosaj.io>
2016-07-08 00:14:53 +0100

    Completed overall functionality
    - Add augmented data to template
    - Generate pdf
    - templates need styling

--- generate.js
+++ generate.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const fs = require('fs');
+const moment = require('moment');
 const Immutable = require('immutable');
 const mustache = require('mustache');
+const colors = require('colors');
 const flags = require('flags');
+const pdf = require('html-pdf');
 
 const runtime = {
   pdfOutput: './out',
@@ -25,9 +28,8 @@ function setup() {
   // Define flags
   flags.defineString('project', 'test', 'The name of the project file without the `.json`');
   flags.parse();
-
   // Create a run object
-  return Immutable.Map({
+  return Immutable.fromJS({
     projectFile: `./projects/${flags.get('project')}.json`,
   });
 }
@@ -40,9 +42,12 @@ function setup() {
 function renderTemplate(project) {
   return new Promise(handler);
   function handler(resolve, reject) {
-    fs.readFile(project.get('template'), 'utf8', (err, data) => {
+    let projectEntry = `./templates/${project.get('template')}/main.html`;
+    fs.readFile(projectEntry, 'utf8', (err, data) => {
       if (err) reject(err);
-      let template = project.set('html', mustache.render(data, project.get('tags')));
+      let tags = project.get('tags').toJS()
+      let html = mustache.render(data, tags);
+      let template = project.set('html', html);
       resolve(template);
     });
   }
@@ -63,7 +68,7 @@ function openProjectFile(path) {
     } catch(e) {
       reject(e);
     }
-    resolve(Immutable.Map(project));
+    resolve(Immutable.fromJS(project));
   }
 }
 
@@ -73,7 +78,41 @@ function openProjectFile(path) {
  * @param {Immutable.Map} project
  */
 function renderPDF(project) {
+  let pdfOpts = {};
+  return new Promise(handler);
 
+  function handler(resolve, reject) {
+    pdf
+      .create(project.get('html'), pdfOpts)
+      .toFile(`${runtime.pdfOutput}/${project.get('name')}.pdf`,
+        (err, res) => {
+          if (err) return reject(err);
+          resolve(project);
+        })
+  }
+}
+
+function augmentInvoice(project) {
+  let todaysDate = moment();
+  let totalCost = 0;
+  project
+    .getIn(['tags', 'work'])
+    .forEach((it) => totalCost += it.get('cost'));
+
+  let work = project
+    .getIn(['tags', 'work'])
+    .map((it) => {
+      let updated = it.set('cost', it.get('cost').toFixed(2));
+      return updated;
+    });
+
+  project = project.mergeIn(['tags', 'work'], work);
+
+  let updatedProject = project.mergeIn(['tags'], {
+    date: todaysDate,
+    totalcost: totalCost,
+  });
+  return updatedProject;
 }
 
 /**
@@ -84,8 +123,15 @@ function renderPDF(project) {
 function generate(settings) {
   let path = settings.get('projectFile');
   openProjectFile(path)
+    .then(augmentInvoice)
     .then(renderTemplate)
-    .then((project) => console.log(project))
-    // .then(renderPDF)
+    // .then(outputProject)
+    .then(renderPDF)
+    .then((project) => console.log('Success!!'.green))
     .catch((err) => console.log(err));
+}
+
+function outputProject(project) {
+  console.log(project);
+  return project;
 }
